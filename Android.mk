9b88dc1bf79dbbc1e98f1ab33230f116df2c40af
Lee Hwangjae <hj-yo.lee@samsung.com>
2012-08-17 12:20:24 +0900

    Support NEON anti-aliasing blit function in SKIA
    original function has calculation are called repetitively, and
    Processed individual pixels only. So Function call overhead was reduced.
    And In contrast to the serial data processing in the existing code,
    the Neon code processes 8 or 4 pixels in parallel. In contrast to the
    serial data processing in the existing code, the Neon code processes
    8 or 4 pixels in parallel
    
    Change-Id: Ib54aeb6bacfd4ed6d257324e8146b211f90b68e9
    Signed-off-by: Lee Hwangjae <hj-yo.lee@samsung.com>

--- Android.mk
+++ Android.mk
@@ -600,6 +600,11 @@ LOCAL_SRC_FILES_arm += \
 	src/opts/SkXfermode_opts_arm.cpp
 
 ifeq ($(ARCH_ARM_HAVE_NEON), true)
+
+LOCAL_CFLAGS += -DNEON_BLITANTIH
+LOCAL_SRC_FILES += \
+	src/core/asm/SkBlitter_RGB16_NEON.S
+
 LOCAL_SRC_FILES_arm += \
 	src/opts/S32A_Opaque_BlitRow32_neon.S \
 	src/opts/S32A_Blend_BlitRow32_neon.S \
